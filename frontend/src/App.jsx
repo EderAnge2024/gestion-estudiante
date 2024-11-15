433f2dd2546297fe636b3acc7b4d3b9ca4ca401b
ClonNetflix <ederangeloingabauitsta@gmail.com>
2024-11-15 15:27:10 -0500

    corregi browseroruter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import HomeComponent from './components/home/HomeComponent';
 import StudentForm from './components/form/StudentForm';
 import StudentList from './components/lists/StudentList';
@@ -10,24 +9,22 @@ import TeacherList from './components/lists/TeacherList';
 import Login from './components/Login/login';
 
 function App() {
- 
-
-  return (
-    <>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomeComponent/>}/>
-        <Route path='/formStudent' element={<StudentForm/>}/>
-        <Route path='/studentList' element={<StudentList/>}/>
-        <Route path='/formteacher' element={<TeacherForm/>}/>
-        <Route path='/teacherList' element={<TeacherList/>}/>
-        <Route path='/login' element={<Login/>}/>
-      
-      </Routes>
-      </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomeComponent />} />
+                    <Route path="/formStudent" element={<StudentForm />} />
+                    <Route path="/studentList" element={<StudentList />} />
+                    <Route path="/formTeacher" element={<TeacherForm />} />
+                    <Route path="/teacherList" element={<TeacherList />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
-export default App
+export default App;
+
 
